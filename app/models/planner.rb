8faa96b52e5dbc2927a3a3755b54304a4f5cf7fe
Moringa School <maryanneanyango98@gmail.com>
2022-09-11 22:21:49 +0300

    files

---
+++ app/models/planner.rb
@@ -0,0 +1,3 @@
+class Planner < ActiveRecord::Base
+    
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+#planners seeding
+planner1 = Planner.create(name: "Yusuf Limun and team", contact:"@teamyusuf")
+planner2 = Planner.create(name: "Abanda P", contact: "@teamabanda")
+planner3 = Planner.create(name: "Jay Dan Lee team", contact: "@jayteam")
+planner4 = Planner.create(name: "DanielTeam", contact: "@Atudolimited")
+
+#events seeding
+event1 = Event.create(name: "wedding dinner", location: "Westland",date: "christmas day")
+event2 = Event.create(name: "conference meeting", location: "Kisumu", date:"easter Friday")
+
+#requests seeding
+request1 = Request.create(name:"Lawi yo", message:"need planners for my wedding", location:"Utawala")
+request2 = Request.create(name:"awino Okeo", message:"need planners for my collection reveal", location:"Utawala")
+request3 = Request.create(name:"wino keyo", message:"need planners for my a party", location:"Utawala")
+request4 = Request.create(name:"Lawio Okyo", message:"need planners for my birthday party", location:"Utawala")
+
+
 
 puts "✅ Done seeding!"
