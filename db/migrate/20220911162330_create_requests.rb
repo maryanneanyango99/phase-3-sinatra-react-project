358200f3cb35fcfdf4cf2795461beef7515ee4a1
Moringa School <maryanneanyango98@gmail.com>
2022-09-13 13:15:25 +0300

    dependencies

--- db/migrate/20220911162330_create_requests.rb
+++ db/migrate/20220911162330_create_requests.rb
@@ -1,9 +1,9 @@
-class CreateRequests < ActiveRecord::Migration[6.1]
-  def change
-    create_table :requests do |t|
-      t.string :name
-      t.string :message
-      t.string :location 
-    end
-  end
-end
+# class CreateRequests < ActiveRecord::Migration[6.1]
+#   def change
+#     create_table :requests do |t|
+#       t.string :name
+#       t.string :message
+#       t.string :location 
+#     end
+#   end
+# end
